3a2eece692ee77fd20c00b4c9dde1ff75e6a3709
ettienemendoza <ettiene.andre@gmail.com>
2025-05-15 11:19:19 -0400

    actualizartodo7

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,21 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '../views/LoginPage.vue';
 import DashboardEjecutivo from '../views/DashboardEjecutivo.vue';
 import DashboardSupervisor from '../views/DashboardSupervisor.vue';
-
 import IncidenciaSupervisor from '../views/IncidenciaSupervisor.vue';
 import IncidenciasLista from '../views/IncidenciasLista.vue';
 import AdministracionUsuarios from '../views/AdministracionUsuarios.vue';
-import ListaSupervisor from '../views/ListaSupervisor.vue'; // Importa el nuevo componente
+import ListaSupervisor from '../views/ListaSupervisor.vue';
 
 const routes = [
   { path: '/', component: LoginPage },
   { path: '/dashboard-ejecutivo', component: DashboardEjecutivo },
   { path: '/dashboard-supervisor', component: DashboardSupervisor },
-  { path: '/incidencia', component: IncidenceForm },
   { path: '/incidencias/:id', component: IncidenciaSupervisor }, // Ruta para el detalle de la incidencia (supervisor)
-  { path: '/incidencias-lista', component: IncidenciasLista }, // Ruta para la lista de incidencias (ejecutivo)
+  { path: '/incidencias-lista', component: IncidenciasLista }, // Ruta para la lista de incidencias (ejecutivo y supervisor)
   { path: '/administracion', component: AdministracionUsuarios },
-  { path: '/lista-supervisor', component: ListaSupervisor }, // Asegúrate de que esta ruta exista si la necesitas
+  { path: '/lista-supervisor', component: ListaSupervisor },
 ];
 
 const router = createRouter({
